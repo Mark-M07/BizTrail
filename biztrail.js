5af4b3d9ef71becfc4ac9991ab02af6557f49b09
Mark-M07 <minehan.mark@gmail.com>
2023-11-15 22:33:44 +1100

    invalid

--- biztrail.js
+++ biztrail.js
@@ -25,7 +25,7 @@ import {
 
 (g => { var h, a, k, p = "The Google Maps JavaScript API", c = "google", l = "importLibrary", q = "__ib__", m = document, b = window; b = b[c] || (b[c] = {}); var d = b.maps || (b.maps = {}), r = new Set, e = new URLSearchParams, u = () => h || (h = new Promise(async (f, n) => { await (a = m.createElement("script")); e.set("libraries", [...r] + ""); for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]); e.set("callback", c + ".maps." + q); a.src = `https://maps.${c}apis.com/maps/api/js?` + e; d[q] = f; a.onerror = () => h = n(Error(p + " could not load.")); a.nonce = m.querySelector("script[nonce]")?.nonce || ""; m.head.append(a) })); d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)) })
     ({ key: "AIzaSyCkI7_eaRpS3YcXXt29lsFCdRy4zUZ59yk", v: "beta" });
-    //({ key: "AIzaSyBJg-GJpfYFVasB5r7kN0yD-WBtCLOcPaM", v: "beta" });
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyD5AQTiAWxG1viyNpPrdQaCPP2fnmZXgvA",
@@ -708,7 +708,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
         } else {
             //console.log("param loc does not exist");
             scanLoading.style.display = 'none';
+            imageSuccess.style.display = 'none';
             imageFail.style.display = 'flex';
+            scanMessage.style.display = 'block';
             scanMessage.textContent = "Invalid QR or code word.";
             scanMessage.style.backgroundColor = '#ffdede';
         }
