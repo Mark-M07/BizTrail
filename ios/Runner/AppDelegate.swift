d17bee9b40a18f3b387a4083fd11f3a83a886c6e
Mark minehan <markminehan@Marks-Mac-mini.local>
2024-12-10 00:50:16 +1100

    ios build

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import Flutter
 import UIKit
 import GoogleMaps
+import FirebaseCore
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,7 +9,12 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Initialize Firebase
+    FirebaseApp.configure()
+    
+    // Initialize Google Maps
     GMSServices.provideAPIKey("AIzaSyCkI7_eaRpS3YcXXt29lsFCdRy4zUZ59yk")
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
